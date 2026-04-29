0e9d00de9fe92a4541ed7cbd22cd16f55f9d97bb
Ashley <ashleysofialee@gmail.com>
2026-04-29 11:54:38 -0500

    completed missing functions

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,27 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p>Please select a show to see episodes.</p>;
+  }
+
+  return (
+    <div className="show-details">
+      <h2>{show.name}</h2>
+      <EpisodeDetails episode={selectedEpisode} />
+
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+    </div>
+  );
 }
